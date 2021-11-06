193dbcb7e7c60ba0da2201843827cb10427bb925
Venkata Yasaswini Reddy <31910543+yashureddykv@users.noreply.github.com>
2021-11-06 14:09:58 -0300

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  6 13:47:33 2021
+
+@author: Yashu
+"""
+
+print("Welcome to the Band Name Generator")
+
+
+a=input("What's the name of the city you grow up in?")
+b=input("What's your pet's name?")
+c=a+" "+b
+print("Your band name could be "+c)
